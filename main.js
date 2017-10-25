f94f4195ea1900441939b70be06e1f2ef41960a9
Rituj Kumar <ritujkumar@hotmail.com>
2017-10-25 08:07:41 -0400

    Events handled

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function saveIssue(e) {
     severity: issueSeverity,
     assignedTo: issueAssigned,
     status: issueStatus,
-  }
+  };
 
   if (localStorage.getItem('issues') == null) {
     var issues = [];
@@ -33,6 +33,32 @@ function saveIssue(e) {
 
 }
 
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+}
+
 function fetchIssues() {
   var issues = JSON.parse(localStorage.getItem('issues'));
   var issuesList = document.getElementById('issueList');
@@ -50,10 +76,14 @@ function fetchIssues() {
                             '<h6> Issue Id: ' + id + '</h6>' +
                             '<p> <span class="label label-info">' + status + '</span></p>' +
                             '<h3>' + desc + '</h3>' +
-                            '<p> <span class="glyphicon glyohicon-time"></span>' + severity + '</p>' +
-                            '<p> <span class="glyphicon glyohicon-user"> </span>' + assignedTo + '</p>' +
-                            '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning"> Close </a>' +
-                            '<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger"> Delete </a>' +
+                            '<p> <span class="glyphicon glyohicon-time"></span>' + severity +
+                            '</p>' +
+                            '<p> <span class="glyphicon glyohicon-user"> </span>' + assignedTo +
+                            '</p>' +
+                            '<a href="#" onclick="setStatusClosed(\'' + id +
+                            '\')" class="btn btn-warning"> Close </a>' +
+                            '<a href="#" onclick="deleteIssue(\'' + id +
+                            '\')" class="btn btn-danger"> Delete </a>' +
                             '</div>';
 
   }
